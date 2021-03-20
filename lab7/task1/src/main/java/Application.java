1fcd242d1019e4b42c14aee66647f0293c632a88
YanaSkyrda <yana.skirda101010@gmail.com>
2021-03-21 01:51:47 +0200

    lab 7 implemented

---
+++ lab7/task1/src/main/java/Application.java
@@ -0,0 +1,43 @@
+import components.DuckAnimation;
+import components.JPanelWithBackground;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Application extends JFrame {
+    private void initializeFrame(JFrame frame, JPanel panel) throws IOException {
+        frame.setContentPane(panel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setTitle("abys mage hunt <3");
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private void initializePanel(JPanel panel) throws IOException {
+        ImageIcon duckIcon = new ImageIcon(
+                new ImageIcon(ImageIO.read(this.getClass().getResource("duck.png")))
+                        .getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+        for (int i = 0; i < 5; i++) {
+            DuckAnimation duck = new DuckAnimation(duckIcon);
+            panel.add(duck);
+            duck.startAction();
+        }
+    }
+
+    private Application() throws IOException {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanelWithBackground(this.getClass().getResource("back.jpg"));
+
+        initializePanel(panel);
+
+        initializeFrame(frame, panel);
+    }
+
+    public static void main(String... args) throws IOException {
+        new Application();
+    }
+}
