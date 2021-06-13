cb7c309524c642262191405c25073aad05b1d0d5
YanaSkyrda <yana.skirda101010@gmail.com>
2021-06-13 19:25:12 +0300

    finished parallel programming course, added old completed tasks

---
+++ courses/parallel-programming/miniproject_4/src/main/java/edu/coursera/parallel/OneDimAveragingPhaser.java
@@ -0,0 +1,151 @@
+package edu.coursera.parallel;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * Wrapper class for implementing one-dimensional iterative averaging using
+ * phasers.
+ */
+public final class OneDimAveragingPhaser {
+    /**
+     * Default constructor.
+     */
+    private OneDimAveragingPhaser() {
+    }
+
+    /**
+     * Sequential implementation of one-dimensional iterative averaging.
+     *
+     * @param iterations The number of iterations to run
+     * @param myNew      A double array that starts as the output array
+     * @param myVal      A double array that contains the initial input to the
+     *                   iterative averaging problem
+     * @param n          The size of this problem
+     */
+    public static void runSequential(final int iterations, final double[] myNew,
+                                     final double[] myVal, final int n) {
+        double[] next = myNew;
+        double[] curr = myVal;
+
+        for (int iter = 0; iter < iterations; iter++) {
+            for (int j = 1; j <= n; j++) {
+                next[j] = (curr[j - 1] + curr[j + 1]) / 2.0;
+            }
+            double[] tmp = curr;
+            curr = next;
+            next = tmp;
+        }
+    }
+
+    /**
+     * An example parallel implementation of one-dimensional iterative averaging
+     * that uses phasers as a simple barrier (arriveAndAwaitAdvance).
+     *
+     * @param iterations The number of iterations to run
+     * @param myNew      A double array that starts as the output array
+     * @param myVal      A double array that contains the initial input to the
+     *                   iterative averaging problem
+     * @param n          The size of this problem
+     * @param tasks      The number of threads/tasks to use to compute the solution
+     */
+    public static void runParallelBarrier(final int iterations,
+                                          final double[] myNew, final double[] myVal, final int n,
+                                          final int tasks) {
+        Phaser ph = new Phaser(0);
+        ph.bulkRegister(tasks);
+
+        Thread[] threads = new Thread[tasks];
+
+        for (int ii = 0; ii < tasks; ii++) {
+            final int i = ii;
+
+            threads[ii] = new Thread(() -> {
+                double[] threadPrivateMyVal = myVal;
+                double[] threadPrivateMyNew = myNew;
+
+                final int chunkSize = (n + tasks - 1) / tasks;
+                final int left = (i * chunkSize) + 1;
+                int right = (left + chunkSize) - 1;
+                if (right > n) right = n;
+
+                for (int iter = 0; iter < iterations; iter++) {
+                    for (int j = left; j <= right; j++) {
+                        threadPrivateMyNew[j] = (threadPrivateMyVal[j - 1]
+                                + threadPrivateMyVal[j + 1]) / 2.0;
+                    }
+                    ph.arriveAndAwaitAdvance();
+
+                    double[] temp = threadPrivateMyNew;
+                    threadPrivateMyNew = threadPrivateMyVal;
+                    threadPrivateMyVal = temp;
+                }
+            });
+            threads[ii].start();
+        }
+
+        for (int ii = 0; ii < tasks; ii++) {
+            try {
+                threads[ii].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * A parallel implementation of one-dimensional iterative averaging that
+     * uses the Phaser.arrive and Phaser.awaitAdvance APIs to overlap
+     * computation with barrier completion.
+     * <p>
+     *
+     * @param iterations The number of iterations to run
+     * @param myNew      A double array that starts as the output array
+     * @param myVal      A double array that contains the initial input to the
+     *                   iterative averaging problem
+     * @param n          The size of this problem
+     * @param tasks      The number of threads/tasks to use to compute the solution
+     */
+    public static void runParallelFuzzyBarrier(final int iterations,
+                                               final double[] myNew, final double[] myVal, final int n,
+                                               final int tasks) {
+        Phaser phaser = new Phaser(tasks);
+
+        Thread[] threads = new Thread[tasks];
+
+        for (int i = 0; i < tasks; i++) {
+            final int curr = i;
+
+            threads[i] = new Thread(() -> {
+                double[] threadMyVal = myVal;
+                double[] threadMyNew = myNew;
+
+                for (int iter = 0; iter < iterations; iter++) {
+                    final int left = curr * (n / tasks) + 1;
+                    threadMyNew[left] = (threadMyVal[left - 1] + threadMyVal[left + 1]) / 2.0;
+
+                    final int right = (curr + 1) * (n / tasks);
+                    threadMyNew[right] = (threadMyVal[right - 1] + threadMyVal[right + 1]) / 2.0;
+
+                    int currentPhase = phaser.arrive();
+
+                    for (int j = left + 1; j <= right - 1; j++) {
+                        threadMyNew[j] = (threadMyVal[j - 1] + threadMyVal[j + 1]) / 2.0;
+                    }
+
+                    phaser.awaitAdvance(currentPhase);
+
+                    double[] temp = threadMyNew;
+                    threadMyNew = threadMyVal;
+                    threadMyVal = temp;
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < tasks; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException ignored) { }
+        }
+    }
+}
